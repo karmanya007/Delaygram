528ba45bbd4fcaf852da8abbf27e47730ab8d486
Karmanya Veer Sharma <karmanyasharma5@gmail.com>
2025-02-08 04:32:59 +0530

    Default theme check fix

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -6,15 +6,16 @@ import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 
 export function ModeToggle() {
-    const { theme, setTheme } = useTheme();
+    const { theme, systemTheme, setTheme } = useTheme();
+
     return (
         <Button
             variant="outline"
             size="icon"
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
-            {theme === "light" && <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />}
-            {theme === "dark" && <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />}
+            {(theme ? theme === "light" : systemTheme === "light") && <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />}
+            {(theme ? theme === "dark" : systemTheme === "dark") && <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />}
         </Button>
     )
 }
